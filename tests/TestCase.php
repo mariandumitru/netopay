450743192b27f2ee54b6a608a08264697e0586b5
mariandumitru <72664506+mariandumitru@users.noreply.github.com>
2026-04-11 19:09:53 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace MarianDumitru\Netopay\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use MarianDumitru\Netopay\NetopayServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
